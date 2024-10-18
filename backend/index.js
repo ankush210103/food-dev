e9ca88db62f9a60ac462603c6774e220c9cc1444
Ankush Kumar <112898190+ankush210103@users.noreply.github.com>
2024-10-18 10:50:45 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+var cors = require('cors')
+const mongoDB = require('./db');
+mongoDB();
+
+app.use(cors())
+
+
+app.use(express.json()); 
+app.use('/api',require("./Routes/CreateUser"));
+app.use('/api',require("./Routes/DisplayData"));
+app.use('/api',require("./Routes/OrderData"));
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+
+ 
+
+app.listen(port,()=>{
+    console.log(`App is listing to port ${port}`);
+    
+})
