8f0bce2cf4014b009e1d5ec109e47aeafdb9251d
Ankush Kumar <112898190+ankush210103@users.noreply.github.com>
2024-10-18 10:49:35 +0530

    Add files via upload

---
+++ backend/models/User.js
@@ -0,0 +1,29 @@
+
+const mongoose = require('mongoose');
+const {Schema}  =require('mongoose');
+
+const UserSchema = new Schema({
+    name:{
+        type: String,
+        require : true
+    },
+    llocation:{
+        type: String,
+        require : true
+    },
+    email:{
+        type: String,
+        require : true
+    },
+    password:{
+        type: String,
+        require : true
+    },
+    date:{
+        type: Date,
+        default: Date.now 
+    }
+
+});
+
+module.exports = mongoose.model('user',UserSchema);
